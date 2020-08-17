8cd5aca3e6210f291bc39a298b06755da57c3dc1
whitedays2007 <win2008summer@gmail.com>
2020-08-17 11:35:57 +0800

    automux

--- script/install.sh
+++ script/install.sh
@@ -1,7 +1,22 @@
-apt update
-apt install git python -y
-pip install termcolor
-git clone https://github.com/whitedays/automux.git $PREFIX/share/AutoMux
-echo "clear\npython $PREFIX/share/AutoMux/automux.py" >$PREFIX/bin/automux
+#!/usr/bin/env bash
+
+printf "\n\033[36mThis is the installation script of automux.\033[0m
+\033[33mDon't use it for Linux System.\033[0m
+\033[34mOnly for Termux!\n\033[0m"
+
+printf "\033[34mPlease Wating 10s!\033[0m\n"
+printf "\033[36mPress Ctrl + C Quit\033[0m"
+
+sleep 10s
+
+apt update && apt upgrade -y
+
+apt install git python
+
+git clone https://github.com/whitedays2007/automux.git $PREFIX/share/AutoMux
+
+echo "clear\npython $PREFIX/share/AutoMux/automux.py" > $PREFIX/bin/automux
+
 chmod 777 $PREFIX/bin/automux
-echo -e "\n\033[31m安装完成，请输入'automux'来启动AutoMux。\033[0m\n"
+
+printf  "\n\033[31m安装完成，请输入'automux'来启动AutoMux。\033[0m\n"
